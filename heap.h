e4b72e2e214445542c9d1061fff3f6847f77cb64
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2023-10-01 15:21:02 -0500

    Add heapify method

--- heap.h
+++ heap.h
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <functional>
 #include <optional>
+#include <stdexcept>
 #include <vector>
 
 template <typename Item>
@@ -15,6 +16,7 @@ class Heap {
         Heap(std::function<Item(Item, Item)> getPriorityItem);
         std::optional<Item> peek() const;
         void push(Item item);
+        void heapify(int index);
         decltype(items.size()) size() const { return items.size(); };
         Item const& operator[](int index) const { return items[index]; };
 };
@@ -46,20 +48,27 @@ std::optional<Item> Heap<Item>::peek() const {
 }
 
 template <typename Item>
-void Heap<Item>::push(Item item) {
-    // First just insert the item at the end of the heap
-    this->items.push_back(item);
-    // Then swim the item up to the root if it violates the heap invariant
-    auto index = this->size() - 1;
+void Heap<Item>::heapify(int index) {
+    if (index < 0 || index >= this->size()) {
+        throw std::invalid_argument("Index out of bounds.");
+    }
     while (index > 0) {
         auto parentIndex = Heap<Item>::getParentIndex(index);
         auto parentItem = this->items[parentIndex];
-        auto priorityItem = this->getPriorityItem(item, parentItem);
+        auto priorityItem = this->getPriorityItem(this->items[index], parentItem);
         if (priorityItem == parentItem) return;
         // Swap parent and item at current index
         std::swap(this->items[index], this->items[parentIndex]);
         // In next iteration, compare parent we just swapped with its parent
         index = parentIndex;
     }
+}
+
+template <typename Item>
+void Heap<Item>::push(Item item) {
+    // First just insert the item at the end of the heap
+    this->items.push_back(item);
+    // Then swim the item up to the root if it violates the heap invariant
+    this->heapify(this->size() - 1);
 }
 
